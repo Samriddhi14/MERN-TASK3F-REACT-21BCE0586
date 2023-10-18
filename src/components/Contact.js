8e0f1f0d6de62a544282d007b42ed7f740539918
Samriddhi14 <sam14.diva@gmail.com>
2023-10-18 22:54:14 +0530

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <p>
+        I'm always open to new opportunities and collaborations. 
+      </p>
+      <ul>
+        <li>
+          <strong>Email:</strong> <a href="sam14.diva@gmail.com">Samriddhi's Email</a>
+        </li>
+        <li>
+          <strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/samriddhi-mishra-879007217/ " target="_blank" rel="noopener noreferrer">Samriddhi's Linkedin</a>
+        </li>
+        <li>
+          <strong>GitHub:</strong> <a href=" https://github.com/Samriddhi14" target="_blank" rel="noopener noreferrer">Samriddhi's GitHub</a>
+        </li>
+       
+      </ul>
+      <p>
+        I'm looking forward to connecting with you!
+      </p>
+    </section>
+  );
+};
+
+export default Contact;
